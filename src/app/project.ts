f95664e3eafa83d052f9038fb7058d1664300739
kalle hålldén <kalle@kalles-MacBook-Pro.local>
2018-10-20 15:28:08 +0200

    Navigation Menu and poject info displayed

---
+++ src/app/project.ts
@@ -0,0 +1,6 @@
+export class Project {
+    id: number;
+    title: String;
+    information: String;
+   // images: [ImageBitmap];
+}
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Project } from '../project';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  exerLogProject: Project = {
+    id: 1,
+    title: "ExerLog",
+    information: "This is an app built in android studio for tracking workouts."
+  }
+
+  ngOnInit() {
+  }
+
+}
